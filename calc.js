da4b6cc79da7b760dbd6e9e93e4e4b3a70041232
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2021-11-29 01:55:05 +0530

    added color coding for the output

--- calc.js
+++ calc.js
@@ -8,25 +8,35 @@ var outputDiv = document.querySelector("#output")
 
 
 function calculateProfitAndLoss(a, b) {
-    var sellingPrice = currentPrice.value;
-    var costPrice = initialPrice.value
-    var sharesQuantity = quantityOfShares.value;
+    
+    var sellingPrice = Number(currentPrice.value);
+    var costPrice = Number(initialPrice.value);
+    var sharesQuantity = Number(quantityOfShares.value);
 
     if (sellingPrice > costPrice) {
         // profit
         var profit = ((sellingPrice - costPrice) * sharesQuantity).toFixed(2)
         var profitPercent = ((profit / costPrice) * 100).toFixed(2)
-
+        
         outputDiv.innerText = "Wow! Congratulations!! You have made a profit of " + profit + " and your profit percentage is " + profitPercent + "%"
+        
+        document.getElementById("output").style.color = "green";
+        
     } else if (sellingPrice === costPrice) {
         // no profit/loss
+        
         outputDiv.innerText = "Hmmm. There is no profit/gain. Remember to be patient!!"
+        
+        document.getElementById("output").style.color = "black";
+
     } else {
         //  loss
         var loss = ((costPrice - sellingPrice) * sharesQuantity).toFixed(2)
         var lossPercent = ((loss / costPrice) * 100).toFixed(2)
-
+        
         outputDiv.innerText = "Ouch! You suffered a loss of " + loss + " and your loss percentage is " + lossPercent + "%"
+        
+        document.getElementById("output").style.color = "red";
     }
 }
 
