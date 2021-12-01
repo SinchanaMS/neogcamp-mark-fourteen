90ab703830a16b73464fbcf724d4dc9cc092110f
SinchanaMS <94738532+SinchanaMS@users.noreply.github.com>
2021-12-01 11:12:06 +0530

    fix: fixed the percentage return logic

--- calc.js
+++ calc.js
@@ -16,7 +16,7 @@ function calculateProfitAndLoss(a, b) {
     if (sellingPrice > costPrice) {
         // profit
         var profit = ((sellingPrice - costPrice) * sharesQuantity).toFixed(2)
-        var profitPercent = ((profit / costPrice) * 100).toFixed(2)
+        var profitPercent = (((sellingPrice-costPrice) / costPrice) * 100).toFixed(2)
         
         outputDiv.innerText = "Wow! Congratulations!! You have made a profit of " + profit + " and your profit percentage is " + profitPercent + "%"
         
@@ -32,7 +32,7 @@ function calculateProfitAndLoss(a, b) {
     } else {
         //  loss
         var loss = ((costPrice - sellingPrice) * sharesQuantity).toFixed(2)
-        var lossPercent = ((loss / costPrice) * 100).toFixed(2)
+        var lossPercent = (((costPrice - sellingPrice) / costPrice) * 100).toFixed(2)
         
         outputDiv.innerText = "Ouch! You suffered a loss of " + loss + " and your loss percentage is " + lossPercent + "%"
         
